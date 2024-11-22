d254b93dfae7e25ac9c48d2836ba17f0bcf03e51
Shahnawaz <airalam786@yahoo.co.in>
2024-11-22 23:54:52 +0530

    playing with strings

--- helloWorld.py
+++ helloWorld.py
@@ -1,1 +1,18 @@
 print ("hello world")
+
+# this line will print in the next line
+print("This is first line and \nthis is the next line")
+
+# this should tab the line
+print("This is an example of tab \tline")
+
+stringOne = "hello world"
+print(len(stringOne)) #this should print the length of the string
+
+# playing with strings
+print(stringOne[1:9]) #should print from 1 to 9th index of string
+print(stringOne[:6])
+print(stringOne[::-1]) #this will reverse the string
+letter = 'z'
+print(letter*10) #this should print z 10 times
+print(len(letter*10)) #this will print the length of the string
