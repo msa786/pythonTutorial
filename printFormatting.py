e32297aa5d2009493e1e6de0058a2ac9a8b439fa
Shahnawaz <airalam786@yahoo.co.in>
2024-11-23 00:14:48 +0530

    adding print formating examples in python

---
+++ printFormatting.py
@@ -0,0 +1,5 @@
+# this should add the string
+string1 = "Random string"
+print("this should add the random string here:", string1)
+# best way to format string
+print("first string: {x} second string: {y} and this number: {z}".format(x="hello", y="Test1", z=27))
