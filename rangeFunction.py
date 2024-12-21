770a68aa701dab9c3e01041de07dbab6bec0a4e7
Shahnawaz <airalam786@yahoo.co.in>
2024-12-21 15:44:10 +0530

    adding range function

---
+++ rangeFunction.py
@@ -0,0 +1,4 @@
+print(list(range(10)))  # Demonstrates range(stop)
+print(list(range(1, 10)))  # Demonstrates range(start, stop)
+print(list(range(1, 10, 2)))  # Demonstrates range(start, stop, step)
+print(list(range(10, 1, -2)))  # Demonstrates range(start, stop, step with a negative value)
