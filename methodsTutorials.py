478cdad0966704d877bb813b83e0dacf5a81fcbb
Shahnawaz <airalam786@yahoo.co.in>
2024-12-25 18:29:59 +0530

    adding the methods python tuts

---
+++ methodsTutorials.py
@@ -0,0 +1,5 @@
+# program to demonstrate the use of methods in python
+listA = [1, 2, 3, 4, 5]
+print(help(listA.count)) #it provides help on the built-in methods and their usages
+listA.insert(3, 10)
+print(listA)
